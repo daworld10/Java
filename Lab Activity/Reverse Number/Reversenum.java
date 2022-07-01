ccbfda8965d9e876e79dae451c4a625fc249d7ff
daworld10 <108351622+daworld10@users.noreply.github.com>
2022-07-01 20:32:00 +0530

    Add files via upload

---
+++ Lab Activity/Reverse Number/Reversenum.java
@@ -0,0 +1,31 @@
+//WAP to accept number from user and display it in reverse order
+package executor;
+
+import java.util.Scanner;  
+public class Reversenum  
+{  
+	//method for reverse a number  
+	static void reverseNumber(int number)   
+	{  
+		if (number < 10)   
+		{  
+			//prints the same number if the number is less than 10  
+			System.out.println(number);  
+			return;  
+		}  
+		else   
+		{  
+			System.out.print(number % 10);  
+			reverseNumber(number/10);  
+		}	  
+	}  
+	public static void main(String args[])  
+	{  
+		System.out.print("Enter the number that you want to reverse: ");  
+		Scanner sc = new Scanner(System.in);  
+		int num = sc.nextInt();  
+		System.out.print("The reverse of the given number is: ");  
+		//method calling  
+		reverseNumber(num);  
+	}  
+}  
